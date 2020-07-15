c00b1d35e35b9fafd0927bf4a1662c6aafb26d5b
Sam Cheung <sacheu@microsoft.com>
2020-07-15 10:02:40 -0700

    Fix bugs in Schema classes

--- Sources/AutorestSwift/Models/OperationGroup.swift
+++ Sources/AutorestSwift/Models/OperationGroup.swift
@@ -13,6 +13,22 @@ public struct OperationGroup: Codable {
 
     public let operations: [Operation]
 
+    enum CodingKeys: String, CodingKey {
+        case operations, key = "$key"
+    }
+    
     // TODO: Apply allOf
     // public let allOf: [Metadata]?
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(key, forKey: .key)
+        try container.encode(operations, forKey: .operations)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        key = try container.decode(String.self, forKey: .key)
+        operations = try container.decode([Operation].self, forKey: .operations)
+    }
 }
