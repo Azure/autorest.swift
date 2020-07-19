99fd6821a09d23947dd56ea3bd78aa31ca978887
Sam Cheung <sacheu@microsoft.com>
2020-07-18 22:14:00 -0700

    Change StringSchema to a class

--- src/AutorestSwift/Models/StringSchema.swift
+++ src/AutorestSwift/Models/StringSchema.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-public typealias StringSchema = Compose<StringSchemaProperty, PrimitiveSchema>
+//public typealias StringSchema = Compose<StringSchemaProperty, PrimitiveSchema>
 
 /// a schema that represents a string value
-public struct StringSchemaProperty: Codable {
+public class StringSchema: Schema {
     /// the maximum length of the string
     public let maxLength: Int?
 
@@ -19,4 +19,29 @@ public struct StringSchemaProperty: Codable {
 
     /// a regular expression that the string must be validated against
     public let pattern: String?
+    
+    public enum CodingKeys: String, CodingKey {
+        case maxLength, minLength, pattern
+    }
+
+    // MARK: Codable
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        maxLength = try? container.decode(Int.self, forKey: .maxLength)
+        minLength = try? container.decode(Int.self, forKey: .minLength)
+        pattern = try? container.decode(String.self, forKey: .pattern)
+
+        try super.init(from: decoder)
+    }
+
+    override public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        if maxLength != nil { try container.encode(maxLength, forKey: .maxLength) }
+        if minLength != nil { try container.encode(minLength, forKey: .minLength) }
+        if pattern != nil { try container.encode(pattern, forKey: .pattern) }
+
+        try super.encode(to: encoder)
+    }
 }
