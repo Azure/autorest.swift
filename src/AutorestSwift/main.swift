4b0f944580766d65c3f92926016c1a254fd14e9b
Sam Cheung <sacheu@microsoft.com>
2020-07-21 12:22:10 -0700

    Create ExtensionStringDecodeKeys interface to have a generic way to specify which keys should be decoded as String in AnyCodable

--- src/AutorestSwift/main.swift
+++ src/AutorestSwift/main.swift
@@ -1,16 +1,16 @@
 import Foundation
 import os.log
 
-import Foundation
-import os.log
+ import Foundation
+ import os.log
 
-guard let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-    fatalError("Unabled to locate Documents directory.")
-}
-let sourceUrl = documentsUrl.appendingPathComponent("code-model-v4-2.yaml")
-let manager = Manager(withInputUrl: sourceUrl, destinationUrl: documentsUrl)
-do {
-    try manager.run()
-} catch {
-    print(error)
-}
+ guard let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+     fatalError("Unabled to locate Documents directory.")
+ }
+ let sourceUrl = documentsUrl.appendingPathComponent("code-model-v4-2.yaml")
+ let manager = Manager(withInputUrl: sourceUrl, destinationUrl: documentsUrl)
+ do {
+     try manager.run()
+ } catch {
+     print(error)
+ }
