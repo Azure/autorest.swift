fc5395b3831c49eebc7091d41cac6f01d48942fc
Sam Cheung <sacheu@microsoft.com>
2020-07-21 12:57:10 -0700

    Rebase to master branch

--- src/AutorestSwift/main.swift
+++ src/AutorestSwift/main.swift
@@ -7,6 +7,9 @@ import os.log
  guard let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
      fatalError("Unabled to locate Documents directory.")
  }
+
+ //let sourceUrl = documentsUrl.appendingPathComponent("communication-chat-code-model-v4-2.yaml")
+ //let sourceUrl = documentsUrl.appendingPathComponent("communication-sms-code-model-v4-2.yaml")
  let sourceUrl = documentsUrl.appendingPathComponent("code-model-v4-2.yaml")
  let manager = Manager(withInputUrl: sourceUrl, destinationUrl: documentsUrl)
  do {
