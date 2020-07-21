67216cdeb2d9e3826dbcdb341bee995d550638ad
Sam Cheung <sacheu@microsoft.com>
2020-07-21 13:01:41 -0700

    Run swiftlint

--- src/AutorestSwift/main.swift
+++ src/AutorestSwift/main.swift
@@ -1,19 +1,19 @@
 import Foundation
 import os.log
 
- import Foundation
- import os.log
+import Foundation
+import os.log
 
- guard let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-     fatalError("Unabled to locate Documents directory.")
- }
+guard let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+    fatalError("Unabled to locate Documents directory.")
+}
 
- //let sourceUrl = documentsUrl.appendingPathComponent("communication-chat-code-model-v4-2.yaml")
- //let sourceUrl = documentsUrl.appendingPathComponent("communication-sms-code-model-v4-2.yaml")
- let sourceUrl = documentsUrl.appendingPathComponent("code-model-v4-2.yaml")
- let manager = Manager(withInputUrl: sourceUrl, destinationUrl: documentsUrl)
- do {
-     try manager.run()
- } catch {
-     print(error)
- }
+// let sourceUrl = documentsUrl.appendingPathComponent("communication-chat-code-model-v4-2.yaml")
+// let sourceUrl = documentsUrl.appendingPathComponent("communication-sms-code-model-v4-2.yaml")
+let sourceUrl = documentsUrl.appendingPathComponent("code-model-v4-2.yaml")
+let manager = Manager(withInputUrl: sourceUrl, destinationUrl: documentsUrl)
+do {
+    try manager.run()
+} catch {
+    print(error)
+}
