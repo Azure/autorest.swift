f776ff1b227eca52242fc38acf22b636a8e7990a
Sam Cheung <sacheu@microsoft.com>
2020-07-27 21:13:43 -0700

    Revert change in main.swift

--- src/AutorestSwift/main.swift
+++ src/AutorestSwift/main.swift
@@ -31,8 +31,7 @@ guard let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .
     fatalError("Unabled to locate Documents directory.")
 }
 
-// let sourceUrl = documentsUrl.appendingPathComponent("code-model-v4-2.yaml")
-let sourceUrl = documentsUrl.appendingPathComponent("communication-chat-0722-code-model-v4-2.yaml")
+let sourceUrl = documentsUrl.appendingPathComponent("code-model-v4-2.yaml")
 let manager = Manager(withInputUrl: sourceUrl, destinationUrl: documentsUrl)
 do {
     try manager.run()
