bfd581a781a556e6faffd71d4611a097ef641207
Sam Cheung <sacheu@microsoft.com>
2020-07-28 00:08:07 -0700

    Apply yaml files to unit test

--- tests/AutorestSwiftTests/AutorestSwiftTests.swift
+++ tests/AutorestSwiftTests/AutorestSwiftTests.swift
@@ -24,34 +24,88 @@
 //
 // --------------------------------------------------------------------------
 
+@testable import AutorestSwiftFramework
 import class Foundation.Bundle
 import XCTest
-@testable import AutorestSwiftFramework
-
 
 final class AutorestSwiftTests: XCTestCase {
     func testSwaggerFromTestServer() throws {
-        
-        let yamlfiles = ["additionalProperties"]
-        
-        for file in yamlfiles {
+        let yamlfiles = [
+            //  "additionalProperties",
+            //  "azure-parameter-grouping",
+            //  "azure-report",
+            //  "azure-resource-x",
+            //  "azure-resource",
+            //  "azure-special-properties",
+            //  "body-array",
+            "body-boolean.quirks",
+            //  "body-boolean",
+            //  "body-byte",
+            //  "body-complex",
+            "body-date",
+            //  "body-datetime-rfc1123",
+            //  "body-datetime",
+            //  "body-dictionary",
+            "body-duration",
+            //  "body-file",
+            "body-formdata-urlencoded",
+            "body-formdata",
+            //  "body-integer",
+            //  "body-number.quirks",
+            //  "body-number",
+            //  "body-string.quirks",
+            //  "body-string",
+            "body-time",
+            //  "complex-model",
+            //  "constants",
+            "custom-baseUrl-more-options",
+            //  "custom-baseUrl-paging",
+            "custom-baseUrl",
+            "extensible-enums-swagger",
+            "head-exceptions",
+            "head",
+            //   "header",
+            //   "httpInfrastructure.quirks",
+            //   "httpInfrastructure",
+            //   "lro",
+            //   "media_types",
+            //   "model-flattening",
+            "multiapi-v1-custom-base-url",
+            //   "multiapi-v1",
+            "multiapi-v2-custom-base-url",
+            "multiapi-v2",
+            //   "multiapi-v3",
+            //   "multiple-inheritance",
+            //   "non-string-enum",
+            //    "object-type",
+            //    "paging",
+            //    "parameter-flattening",
+            //    "report",
+            //    "required-optional",
+            //    "storage",
+            "subscriptionId-apiVersion"
+            //  "url-multi-collectionFormat",
+            //  "url",
+            //   "validation",
+            //    "xml-service",
+            //    "xms-error-responses"
+        ]
 
-            let result = runTestSwaggerFromTestServer(yamlFileName: file)
-            
-            if (!result) {
-                print("*** \(file) return false")
-            }
-            // XCTAssertTrue(try runTestSwaggerFromTestServer(yamlFileName: file))
+        for file in yamlfiles {
+            XCTAssertTrue(runTestSwaggerFromTestServer(yamlFileName: file))
         }
     }
 
     func runTestSwaggerFromTestServer(yamlFileName: String) -> Bool {
-       let bundle =  Bundle(for: type(of: self))
-        guard let destUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("generated").appendingPathComponent("test") else  {
+        print("Test \(yamlFileName).yaml")
+
+        let bundle = Bundle(for: type(of: self))
+        guard let destUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?
+            .appendingPathComponent("generated").appendingPathComponent("test") else {
             print("\(yamlFileName).yaml destUrl is nil")
             return false
         }
-            
+
         if let sourceUrl = bundle.url(forResource: yamlFileName, withExtension: "yaml") {
             let manager = Manager(withInputUrl: sourceUrl, destinationUrl: destUrl)
             do {
@@ -59,18 +113,15 @@ final class AutorestSwiftTests: XCTestCase {
                 return isMatchedConsistency
             } catch {
                 print(error)
-                 return false
+                return false
             }
-        }
-        else {
-        
-        
+        } else {
             print("\(yamlFileName).yaml fails to load from bundle")
             return false
         }
     }
-    
+
     static var allTests = [
-        ("testSwaggerFromTestServer")
+        "testSwaggerFromTestServer"
     ]
 }
