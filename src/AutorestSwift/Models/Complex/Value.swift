b002f86c58a4df790615d812ac16c846bc948957
Sam Cheung <sacheu@microsoft.com>
2020-08-04 13:05:58 -0700

    Remove custom encoder/decoder in Value

--- src/AutorestSwift/Models/Complex/Value.swift
+++ src/AutorestSwift/Models/Complex/Value.swift
@@ -87,40 +87,4 @@ class Value: Codable, LanguageShortcut {
         case `protocol`
         case extensions
     }
-
-    /*
-        public required init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            schema = try container.decode(Schema.self, forKey: .schema)
-            _required = try container.decode(Bool.self, forKey: ._required)
-            nullable = try container.decode(Bool.self, forKey: .nullable)
-            assumedValue = try container.decode(String.self, forKey: .assumedValue)
-            clientDefaultValue = try container.decode(String.self, forKey: .clientDefaultValue)
-            summary = try container.decode(String.self, forKey: .summary)
-            apiVersions = try container.decode([ApiVersion].self, forKey: .apiVersions)
-            deprecated = try container.decode(Deprecation.self, forKey: .deprecated)
-            origin = try container.decode(String.self, forKey: .origin)
-            externalDocs = try container.decode(ExternalDocumentation.self, forKey: .externalDocs)
-            language = try container.decode(Languages.self, forKey: .language)
-            `protocol` = try container.decode(Protocols.self, forKey: .protocol)
-            extensions = try container.decode(AnyCodable.self, forKey: .extensions)
-        }
-
-        public func encode(to encoder: Encoder) throws {
-            var container = encoder.container(keyedBy: CodingKeys.self)
-            try container.encode(schema, forKey: .schema)
-            try container.encode(_required, forKey: ._required)
-            try container.encode(nullable, forKey: .nullable)
-            try container.encode(assumedValue, forKey: .assumedValue)
-            try container.encode(clientDefaultValue, forKey: .clientDefaultValue)
-            try container.encode(summary, forKey: .summary)
-            try container.encode(apiVersions, forKey: .apiVersions)
-            try container.encode(deprecated, forKey: .deprecated)
-            try container.encode(origin, forKey: .origin)
-            try container.encode(externalDocs, forKey: .externalDocs)
-            try container.encode(language, forKey: .language)
-            try container.encode(`protocol`, forKey: .protocol)
-            try container.encode(extensions, forKey: .extensions)
-        }
-     */
 }
