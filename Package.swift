796b4106c7bb24c80d9d6e38e75469fb00ddb67f
Sam Cheung <sacheu@microsoft.com>
2020-08-13 18:27:00 -0700

    Fix issue of swift build can't compile

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "3.0.1"),
-        .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.13.1")
+        .package(url: "https://github.com/stencilproject/Stencil.git", .branch("trim_whitespace"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
