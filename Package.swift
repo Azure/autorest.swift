7e23caa19840d26d903d3a3ed07ecaf08727c6a5
Sam Cheung <sacheu@microsoft.com>
2020-08-26 22:59:43 -0700

    Add .testTarget in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,13 +6,15 @@ import PackageDescription
 let package = Package(
     name: "AutorestSwift",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/jpsim/Yams.git", from: "3.0.1"),
         .package(url: "https://github.com/stencilproject/Stencil.git", .branch("trim_whitespace")),
-        .package(url: "https://github.com/apple/swift-nio", from: "2.0.0")
+        .package(url: "https://github.com/apple/swift-nio", from: "2.0.0"),
+        .package(name: "AzureSDK", url: "https://github.com/Azure/azure-sdk-for-ios.git", .branch("master")),
+        .package(name: "AutoRestHeadTest", path: "./test/integration/generated/head/")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,6 +27,11 @@ let package = Package(
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOFoundationCompat", package: "swift-nio")
             ]
+        ),
+        .testTarget(
+            name: "AutorestSwiftTest",
+            dependencies: [.product(name: "AzureCore", package: "AzureSDK"), "AutoRestHeadTest"],
+            path: "AutorestSwiftTest"
         )
     ],
     swiftLanguageVersions: [.v5]
