c2c73202253aef274e10d8159a90675fd2dd64cd
Sam Cheung <sacheu@microsoft.com>
2020-08-27 20:58:15 -0700

    Add body-file.json generated code

--- scripts/testserver_codegen.sh
+++ scripts/testserver_codegen.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-autorest --verbose --input-file=./node_modules/@microsoft.azure/autorest.testserver/swagger/head.json --output-folder=./test/integration/generated/head --namespace=head --use=.
-cd ./test/integration/generated/head
-swift build
-swift package generate-xcodeproj
-cd ../../../..
+make install
+
+files=( "head" "body-file")
+for i in "${files[@]}"
+do
+    autorest --input-file=./node_modules/@microsoft.azure/autorest.testserver/swagger/$i.json --output-folder=./test/integration/generated/$i --namespace=$i --use=.
+    cd ./test/integration/generated/$i
+    swift build
+    swift package generate-xcodeproj
+    cd ../../../..
+done
