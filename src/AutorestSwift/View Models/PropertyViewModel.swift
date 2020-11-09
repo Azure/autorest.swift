20c0690997b0368d0e3a0a93555bfe7685950966
Sam Cheung <sacheu@microsoft.com>
2020-11-09 08:48:37 -0800

    Refactor

--- src/AutorestSwift/View Models/PropertyViewModel.swift
+++ src/AutorestSwift/View Models/PropertyViewModel.swift
@@ -41,12 +41,14 @@ struct PropertyViewModel {
     let isDate: Bool
     let optional: Bool
     let className: String
+    let serializedName: String?
 
     /// Initialize from Value type (such as Property or Parameter)
-    init(from schema: Value) {
+    init(from schema: Property) {
         let name = schema.serializedName ?? schema.name
         assert(!name.isEmpty)
         self.name = name
+        self.serializedName = schema.serializedName
         self.comment = ViewModelComment(from: schema.description)
         self.className = schema.schema!.swiftType()
         self.optional = !schema.required
