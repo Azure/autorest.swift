9ef8321d3741620ef35ccd106781acf036a5891e
Sam Cheung <sacheu@microsoft.com>
2020-11-10 14:23:44 -0800

    Address PR feedback

--- src/AutorestSwift/View Models/OperationViewModel.swift
+++ src/AutorestSwift/View Models/OperationViewModel.swift
@@ -27,13 +27,13 @@
 import Foundation
 
 struct OperationParameters {
-    var header: Params
-    var query: Params
-    var path: [KeyValueViewModel]
-    var body: BodyParams?
-    var signature: [ParameterViewModel]
-    var hasOptionalParams: Bool
-    var methodDecoding: [KeyValueViewModel]
+    let header: Params
+    let query: Params
+    let path: [KeyValueViewModel]
+    let body: BodyParams?
+    let signature: [ParameterViewModel]
+    let hasOptionalParams: Bool
+    let methodDecoding: [KeyValueViewModel]
 
     /// Build a list of required and optional query params and headers from a list of parameters
     init(parameters: [ParameterType], operation: Operation) {
@@ -132,9 +132,9 @@ enum BodyParamStrategy: String {
 }
 
 struct BodyParams {
-    var param: ParameterViewModel
-    var strategy: String
-    var children: [VirtualParam]
+    let param: ParameterViewModel
+    let strategy: String
+    let children: [VirtualParam]
 
     var flattened: Bool {
         return !children.isEmpty
