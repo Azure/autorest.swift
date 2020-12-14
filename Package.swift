564f4a8dc65b147cc0471343791876a3da378ea0
Travis Prescott <trpresco@microsoft.com>
2020-12-14 11:35:13 -0800

    Retarget AzureCore master branch.

--- Package.swift
+++ Package.swift
@@ -13,12 +13,11 @@ let package = Package(
         .package(url: "https://github.com/tjprescott/Yams.git", .branch("CircularReferences")),
         .package(url: "https://github.com/stencilproject/Stencil.git", .branch("trim_whitespace")),
         .package(url: "https://github.com/apple/swift-nio", from: "2.0.0"),
-        .package(name: "AzureSDK", path: "../azure-sdk-for-ios"),
-//        .package(
-//            name: "AzureSDK",
-//            url: "https://github.com/Azure/azure-sdk-for-ios.git",
-//            .branch("dev/AzureCore")
-//        ),
+        .package(
+            name: "AzureSDK",
+            url: "https://github.com/Azure/azure-sdk-for-ios.git",
+            .branch("master")
+        ),
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.45.6"),
         .package(url: "https://github.com/realm/SwiftLint.git", from: "0.40.1"),
         .package(name: "AutoRestHeadTest", path: "./test/integration/generated/head/"),
