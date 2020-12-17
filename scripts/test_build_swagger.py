f719694d643646624820359c754882a859777fc9
Travis Prescott <tjprescott@users.noreply.github.com>
2020-12-17 07:54:57 -0800

    Update ci.yml

--- scripts/test_build_swagger.py
+++ scripts/test_build_swagger.py
@@ -3,6 +3,7 @@
 import json
 import logging
 import os
+import shutil
 import subprocess
 import sys
 import tempfile
@@ -13,7 +14,6 @@ end_color = '\033[0m'
 repos_to_clone = ['azure-rest-api-specs']
 
 cwd = os.getcwd()
-rest_api_specs_path = r'/Users/travisprescott/repos/azure-rest-api-specs'
 
 
 class SwaggerInfo:
@@ -57,7 +57,7 @@ def log_error_and_quit(msg):
 
 def generate(item, *, src, dest):
     """ Generate the SDK for the SwaggerInfo object and return the output. """
-    print(f'== Generate code for {item.namespace}.{item.name} ==\n')
+    print(f'Generating: {item.namespace}.{item.name}...')
     command = f'autorest --input-file={src} --output-folder={dest} --namespace={item.namespace} --use={cwd} --package-name={item.name}CI'
     out, err = run_command(command)
     if err:
@@ -68,24 +68,22 @@ def generate(item, *, src, dest):
 def main(argv):
 
     swagger_data = load_swagger_info()
-
     with tempfile.TemporaryDirectory() as tempdir:
+        rest_api_specs_path = f'{tempdir}/azure-rest-api-specs'
         for repo in repos_to_clone:
-            print(f"==Cloning '{repo}'==")
-            out, err = run_command(f'git clone https://github.com/Azure/{repo}.git {tempdir}')
+            print(f'Cloning: {repo}...')
+            out, err = run_command(f'git clone https://github.com/Azure/{repo}.git {rest_api_specs_path}')
 
         for item in swagger_data:
             src = os.path.join(rest_api_specs_path, item.path)
             dest = os.path.join(tempdir, 'generated', item.namespace, f'{item.name}CI')
             out = generate(item, src=src, dest=dest)
-            print(f'== Build package for {item.namespace}.{item.name} ==\n')
+            print(f'Building: {item.namespace}.{item.name}...')
             out, err = run_command(f'swift build --package-path={dest}')
             if err:
                 log_error_and_quit(err)
-
-    print('== PACKAGES GENERATED AND BUILT SUCCESSFULLY! ==')
-    sys.exit(0)
-
+        print('== PACKAGES GENERATED AND BUILT SUCCESSFULLY! ==')
+        sys.exit(0)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
