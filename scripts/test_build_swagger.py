0f5615c454bc6044a2b16bce01ec5d9c6f28b178
Travis Prescott <trpresco@microsoft.com>
2021-02-16 11:17:36 -0800

    Update script.

--- scripts/test_build_swagger.py
+++ scripts/test_build_swagger.py
@@ -41,53 +41,49 @@ def load_swagger_info():
 
 def run_command(command):
     """ Run a command in a subprocess and return stderr and stdout. """
-    proc = subprocess.Popen(
-        command.split(' '),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    return proc.communicate()
+    result = subprocess.run(command.split(' '), capture_output=True)
+    return (result.stdout, result.stderr, result.returncode)
 
 
-def log_error_and_quit(msg):
+def log_error_and_quit(msg, out, err, code):
     """ Log an error message and exit. """
-    logging.error(f'{warning_color}{msg}{end_color}')
-    sys.exit(1)
+    if err:
+        logging.error(f'{err}')
+    elif out:
+        logging.error(f'{out}')
+    logging.error(f'{warning_color}{msg} ({code}){end_color}')
+    sys.exit(code)
 
 
 def generate(item, *, src, dest):
     """ Generate the SDK for the SwaggerInfo object and return the output. """
     print(f'Generating: {item.namespace}.{item.name}...')
     command = f'autorest --input-file={src} --output-folder={dest} --namespace={item.namespace} --use={cwd} --package-name={item.name}CI'
-    out, err = run_command(command)
-#    if err:
-#        log_error_and_quit(err)
+    out, err, code = run_command(command)
+    if code:
+        log_error_and_quit(f'Error generating: {item.namespace}.{item.name}', out, err, code)
     return out
 
 
 def main(argv):
-
-    # determine Node version
-    out, _ = run_command('node --version')
-    print(f'Node.js version: {out}')
 
     swagger_data = load_swagger_info()
     with tempfile.TemporaryDirectory() as tempdir:
         rest_api_specs_path = f'{tempdir}/azure-rest-api-specs'
         for repo in repos_to_clone:
             print(f'Cloning: {repo}...')
-            out, err = run_command(f'git clone https://github.com/Azure/{repo}.git {rest_api_specs_path}')
+            out, err, code = run_command(f'git clone https://github.com/Azure/{repo}.git {rest_api_specs_path}')
 
         for item in swagger_data:
             src = os.path.join(rest_api_specs_path, item.path)
             dest = os.path.join(tempdir, 'generated', item.namespace, f'{item.name}CI')
             out = generate(item, src=src, dest=dest)
             print(f'Building: {item.namespace}.{item.name}...')
-            out, err = run_command(f'swift build --package-path={dest}')
-            if err:
-                log_error_and_quit(err)
+            out, err, code = run_command(f'swift build --package-path={dest}')
+            if code:
+                log_error_and_quit(f'Error building {item.namespace}.{item.name}', out, err, code)
         print('== PACKAGES GENERATED AND BUILT SUCCESSFULLY! ==')
-        sys.exit(0)
+        sys.exit(code)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
