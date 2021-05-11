a40ee17a34979ef6cd5b27630fc3804d0a3c7353
Travis Prescott <tjprescott@users.noreply.github.com>
2021-05-11 14:02:23 -0700

    Set swift-tools-version to 5.0 (#313)
    * Set swift-tools-version to 5.0.
    
    * Set generated Package.swift to 5.0.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AutorestSwift",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_14)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
